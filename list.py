926c56b0a64c2fa5d6f92999d068632f56220c12
Diya <148466419+Diya0077@users.noreply.github.com>
2024-08-26 14:22:03 +0530

    added list

---
+++ list.py
@@ -0,0 +1,24 @@
+my_list = ["Akash","Debashruti",420,418,"Lovers",True]
+l1 = [4,7,8,90,100,1,2,3,66,59]
+
+popped_element = my_list.pop(2)
+print("Popped Element:", popped_element)
+
+length = len(my_list)
+print("Length of the list:", length)
+
+index = my_list.append(5)
+print("Updated List:", my_list)
+
+
+count = l1.count(4)
+print(count)
+
+l1.sort()
+print("Sorted List:", l1)
+
+l1.reverse()
+print("Reversed List:", l1)
+
+my_list.clear()
+print("Cleared List:", my_list)
